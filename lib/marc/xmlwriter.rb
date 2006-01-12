77bf9107b46440c6380b9cc095894d891acaeb11
ksclarke <ksclarke@4dc5e89f-90f6-0310-ab54-a6a856e7c30e>
2006-01-12 04:10:11 +0000

    Removed namespace from individual records in favor of default namespace on the collection element
    git-svn-id: http://marc.rubyforge.org/svn/ruby-marc/trunk@293 4dc5e89f-90f6-0310-ab54-a6a856e7c30e

--- lib/marc/xmlwriter.rb
+++ lib/marc/xmlwriter.rb
@@ -20,7 +20,7 @@ module MARC
       
       @fh.write("<?xml version='1.0'?>")
       
-      @fh.write("<marc:collection xmlns:marc='" + MARC_NS + "' " +
+      @fh.write("<collection xmlns='" + MARC_NS + "' " +
         "xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " +
         "xsi:schemaLocation='" + MARC_NS + " " + MARC_XSD + "'>")
     end
@@ -36,7 +36,7 @@ module MARC
     # close underlying filehandle
     
     def close
-      @fh.write("</marc:collection>")
+      @fh.write("</collection>")
       @fh.close
     end
 
@@ -45,19 +45,19 @@ module MARC
     # and returns a REXML::Document for the XML serialization
 
     def self.encode(record)
-      root = "<marc:record xmlns:marc='" + MARC_NS + "'/>"
+      root = "<record/>"
       doc = REXML::Document.new root
 
       # MARCXML is particular about this; ILSes aren't
       record.leader[20..24] = "4500"
       
-      leader = REXML::Element.new "marc:leader"
+      leader = REXML::Element.new "leader"
       leader.add_text record.leader
       doc.root.add_element leader
       
       for field in record.fields
         if field.class == MARC::DataField 
-          datafield_elem = REXML::Element.new "marc:datafield"
+          datafield_elem = REXML::Element.new "datafield"
           datafield_elem.add_attributes({
             "tag"=>field.tag,
             "ind1"=>field.indicator1,
@@ -65,7 +65,7 @@ module MARC
           })
 
           for subfield in field.subfields
-            subfield_element = REXML::Element.new "marc:subfield"
+            subfield_element = REXML::Element.new "subfield"
             subfield_element.add_attribute("code", subfield.code)
             subfield_element.add_text subfield.value
             datafield_elem.add_element subfield_element
@@ -73,7 +73,7 @@ module MARC
           
           doc.root.add_element datafield_elem
         elsif field.class == MARC::ControlField
-          control_element = REXML::Element.new "marc:controlfield"
+          control_element = REXML::Element.new "controlfield"
           control_element.add_attribute("tag", field.tag)
           control_element.add_text field.value
           doc.root.add_element control_element
