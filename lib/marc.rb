f051ee7f11b4845eb7359183de1a1783434f460a
ed <ed@4dc5e89f-90f6-0310-ab54-a6a856e7c30e>
2006-04-21 16:38:19 +0000

    doc fixes
    git-svn-id: http://marc.rubyforge.org/svn/ruby-marc/trunk@501 4dc5e89f-90f6-0310-ab54-a6a856e7c30e

--- lib/marc.rb
+++ lib/marc.rb
@@ -1,3 +1,30 @@
+#marc is a ruby library for reading and writing MAchine Readable Cataloging
+#(MARC). More information about MARC can be found at <http://www.loc.gov/marc>.
+#
+#USAGE 
+#
+#    require 'marc'
+#
+#    # reading records from a batch file
+#    reader = MARC::Reader.new('marc.dat')
+#    for record in reader
+#      puts record['245']['a']
+#    end
+#
+#    # creating a record 
+#    record = MARC::Record.new()
+#    record.add_field(MARC::DataField.new('100', '0',  ' ', ['a', 'John Doe']))
+#
+#    # writing a record
+#    writer = MARC::Writer.new('marc.dat')
+#    writer.write(record)
+#    writer.close()
+#
+#    # writing a record as XML
+#    writer = MARC::XMLWriter.new('marc.xml')
+#    writer.write(record)
+#    writer.close()
+
 require 'marc/constants'
 require 'marc/record'
 require 'marc/datafield'
