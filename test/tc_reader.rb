c6c93b4f70569f84837aef9ed1f451ebb1c6503b
ed <ed@4dc5e89f-90f6-0310-ab54-a6a856e7c30e>
2007-05-01 15:48:02 +0000

    release 0.1.5
    git-svn-id: http://marc.rubyforge.org/svn/ruby-marc/trunk@799 4dc5e89f-90f6-0310-ab54-a6a856e7c30e

--- test/tc_reader.rb
+++ test/tc_reader.rb
@@ -3,36 +3,41 @@ require 'marc'
 
 class ReaderTest < Test::Unit::TestCase
 
-    def test_batch
-        reader = MARC::Reader.new('test/batch.dat')
-        count = 0
-        reader.each { count += 1 }
-        assert_equal(count, 10)
-    end
+  def test_batch
+    reader = MARC::Reader.new('test/batch.dat')
+    count = 0
+    reader.each { count += 1 }
+    assert_equal(count, 10)
+  end
 
-    def test_loose
-        reader = MARC::ForgivingReader.new('test/batch.dat')
-        count = 0
-        reader.each { count += 1 }
-        assert_equal(10, count)
-    end
+  def test_loose
+    reader = MARC::ForgivingReader.new('test/batch.dat')
+    count = 0
+    reader.each { count += 1 }
+    assert_equal(10, count)
+  end
 
-    def test_search
-        reader = MARC::Reader.new('test/batch.dat')
-        records = reader.find_all { |r| r =~ /Perl/ }
-        assert_equal(10, records.length)
+  def test_bad_marc
+    reader = MARC::Reader.new('test/tc_reader.rb')
+    record = reader.entries[0]
+  end
 
-        reader = MARC::Reader.new('test/batch.dat')
-        records = reader.find_all { |r| r['245'] =~ /Perl/ }
-        assert_equal(10, records.length)
+  def test_search
+    reader = MARC::Reader.new('test/batch.dat')
+    records = reader.find_all { |r| r =~ /Perl/ }
+    assert_equal(10, records.length)
 
-        reader = MARC::Reader.new('test/batch.dat')
-        records = reader.find_all { |r| r['245']['a'] =~ /Perl/ }
-        assert_equal(10, records.length)
+    reader = MARC::Reader.new('test/batch.dat')
+    records = reader.find_all { |r| r['245'] =~ /Perl/ }
+    assert_equal(10, records.length)
 
-        reader = MARC::Reader.new('test/batch.dat')
-        records = reader.find_all { |r| r =~ /Foo/ }
-        assert_equal(0, records.length)
-    end
+    reader = MARC::Reader.new('test/batch.dat')
+    records = reader.find_all { |r| r['245']['a'] =~ /Perl/ }
+    assert_equal(10, records.length)
+
+    reader = MARC::Reader.new('test/batch.dat')
+    records = reader.find_all { |r| r =~ /Foo/ }
+    assert_equal(0, records.length)
+  end
 
 end
