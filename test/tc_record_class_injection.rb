2a49cc528b9d437ba40d5c99262d87ef065663fa
Bill Dueber <bill@dueber.com>
2014-12-10 23:04:30 -0500

    Added record_class injection to xml parsers and reader, as well as test

--- test/tc_record_class_injection.rb
+++ test/tc_record_class_injection.rb
@@ -1,10 +1,15 @@
 require 'test/unit'
 require 'marc/record'
 require 'marc/reader'
+require 'marc/xmlreader'
 
 class MyRecord < MARC::Record
   def isbn
     self['020'] and self['020']['a']
+  end
+
+  def lccn
+    self['010'] and self['010']['a']
   end
 end
 
@@ -23,6 +28,19 @@ class RecordClassInjectionTest < MiniTest::Unit::TestCase
     reader = MARC::Reader.new('test/one.dat', :record_class => MyRecord)
     r = reader.first
     assert_equal("0471383147 (paper/cd-rom : alk. paper)", r.isbn)
+  end
+
+  def test_xml_injection
+    reader = MARC::XMLReader.new('test/one.xml')
+    r = reader.first
+    assert_raises(NoMethodError) do
+      r.isbn
+    end
+
+    reader = MARC::XMLReader.new('test/one.xml', :record_class => MyRecord)
+    r = reader.first
+    assert_equal("afc99990058366", r.lccn)
+
   end
 
 end
