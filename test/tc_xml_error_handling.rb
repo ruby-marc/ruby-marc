35d1a948d41ac4581e0260ec8ef5d0e784fc3518
Adam Constabaris <adjam@noreply.github.com>
2019-03-26 13:47:26 -0400

    omit xml error tests under Jruby/nokogiri before 1.10.2

--- test/tc_xml_error_handling.rb
+++ test/tc_xml_error_handling.rb
@@ -9,7 +9,7 @@ class BadXMLHandlingTestCase < Test::Unit::TestCase
     rescue LoadError
       omit("nokogiri not installed, cannot test")
     end
-    omit("nokogiri under jruby doesn't support error handling: sparklemotion/nokogiri#1847") if RUBY_PLATFORM == 'java'
+    omit("nokogiri (<1.10.2) under jruby doesn't support error handling: sparklemotion/nokogiri#1847") if RUBY_PLATFORM == 'java' && Gem::Version.new(Nokogiri::VERSION) < Gem::Version.new('1.10.2')
     count = 0
     reader = MARC::XMLReader.new('test/three-records-second-bad.xml', :parser => :nokogiri)
     assert_raise MARC::XMLParseError do
