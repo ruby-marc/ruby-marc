e74b4582ada2bb948da69d926013b7a151da907e
Bill Dueber <bill@dueber.com>
2023-01-19 12:39:13 -0500

    Begin OAI overhaul -- in progress

--- lib/marc/dublincore.rb
+++ lib/marc/dublincore.rb
@@ -2,6 +2,97 @@ module MARC
   # A class for mapping MARC records to Dublin Core
 
   class DublinCore
+
+    class DataFieldExtractor
+      attr_accessor :tag, :ind1, :ind2, :include_codes, :exclude_codes
+
+      def initialize(tag:, ind1: nil, ind2: nil, include_codes: [], exclude_codes: [])
+        @tag = parse_value(tag)
+        @ind1 = parse_value(ind1)
+        @ind2 = parse_value(ind2)
+        @include_codes = include_codes.kind_of?(Array) ? include_codes : include_codes.chars
+        @exclude_codes = exclude_codes.kind_of?(Array) ? exclude_codes : exclude_codes.chars
+        yield self if block_given?
+      end
+
+      def parse_value(tag)
+        case tag
+          when Numeric
+            tag.to_s
+          when Array
+            Regexp.union(tag)
+          else
+            tag
+        end
+      end
+
+      # @param [MARC::DataField] field
+      def indicators_match?(field)
+        ind1match = ind1.nil? or (ind1 === field.indicator1)
+        ind2match = ind2.nil? or (ind2 === field.indicator2)
+        ind1match and ind2match
+      end
+
+      def sfmatch?(sf)
+        (include_codes.empty? or include_codes.include?(sf.code)) and
+          (exclude_codes.empty? or !exclude_codes.include?(sf.code))
+      end
+
+      # @param [MARC::DataField] field
+      def includes?(field)
+        tag === field.tag and indicators_match?(field)
+      end
+
+      # @param [MARC::DataField] field
+      def extract(field)
+        return nil unless self.includes?(field)
+        field.subfields.select{|sf| sfmatch?(sf)}.map(&:value).join(" ").gsub(/\s+/, ' ').gsub(/\A(.*?)[;\/]+\Z/, '\1').strip
+      end
+    end
+
+    class DCFieldSpec
+      attr_reader :tagmatcher
+      def initialize(*datafield_specs)
+        @datafield_specs = datafield_specs
+        @tagmatcher = Regexp.union(@datafield_specs.flatten.map(&:tag))
+      end
+
+      #@param [MARC::Record} record]
+      def extract(record)
+        candidates = record.fields.select{|f| @tagmatcher.match?(f.tag)}
+        candidates.flat_map{|f| @datafield_specs.map{|dfspec| dfspec.extract(f)}}.compact
+      end
+    end
+
+    # MAPPING ARE TAKEN FROM https://www.loc.gov/marc/marc2dc.html
+    # WITH SOME CODE RESTRICTIONS
+    DCSPECS = {
+      'title' =>  DCFieldSpec.new(DataFieldExtractor.new(tag: %w(245 246), include_codes: "abdefgknp")),
+
+      "creator" => DCFieldSpec.new(
+        DataFieldExtractor.new(tag: %w(100 700), include_codes: "abcdjq"),
+        DataFieldExtractor.new(tag: %w(110 710), include_codes: "abcd"),
+        DataFieldExtractor.new(tag: %w(111 711), include_codes: "acden")),
+
+      "subject" => DCFieldSpec.new(
+        DataFieldExtractor.new(tag: %w(600 610 611 630), include_codes: "abcdefghjklmnopqrstuvxyz"),
+        DataFieldExtractor.new(tag: %w(650 653), include_codes: "abkcdevxyz")
+      ),
+
+      "description" =>  DCFieldSpec.new(
+        DataFieldExtractor.new(tag: /5\d\d/)
+      ),
+
+      "publisher" => CFieldSpec.new(
+        DataFieldExtractor.new(tag: "260", include_codes: "ab")
+      ),
+
+      "date" => CFieldSpec.new(
+        DataFieldExtractor.new(tag: "260", include_codes: "cg")
+      ),
+    }
+
+
     def self.map(record)
       dc_hash = {}
       dc_hash["title"] = get_field_value(record["245"]["a"])
